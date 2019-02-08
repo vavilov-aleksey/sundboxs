345308fec057d839c1034d019086dbe2fa02785e
vavilov-aleksey <33984766+vavilov-aleksey@users.noreply.github.com>
2019-02-08 10:50:13 +0300

    Create position-fixed-prodlem.js

---
+++ position-fixed-prodlem.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Проблема: нужно сделать position:fixed и блок прилипает к правому скролу и накладывается на него
+// Решение:
+export default class Footer {
+  constructor(){
+    const site = document.querySelector('.site');
+
+    if(site){
+      let siteNotify = document.querySelector('.site__notify');
+      if(siteNotify) this.widthScroll(site, siteNotify)
+    }
+  }
+
+  widthScroll(parent, element){
+    this.scrollWidth = parent.offsetWidth - parent.clientWidth;
+    this.siteNotify = element;
+
+    if(this.siteNotify) this.siteNotify.style.right = `${this.scrollWidth}px`;
+  }
+}
