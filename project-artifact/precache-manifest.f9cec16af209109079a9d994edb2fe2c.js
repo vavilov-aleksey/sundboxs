9b9ad79856e2bd1974e873fa7783428e94691a07
Aleksey Vavilov <aleksey_vavilov@mail.ru>
2019-03-10 19:14:55 +0400

    edit link

--- project-artifact/precache-manifest.f9cec16af209109079a9d994edb2fe2c.js
+++ project-artifact/precache-manifest.f9cec16af209109079a9d994edb2fe2c.js
@@ -1,70 +1,70 @@
 self.__precacheManifest = [
   {
     "revision": "99cc44826059be3b12429af3e8c564c9",
-    "url": "/static/media/creep.99cc4482.svg"
+    "url": "/project-artifact/static/media/creep.99cc4482.svg"
   },
   {
     "revision": "5e42fc3efdcc79a7cb1d",
-    "url": "/static/css/main.33744029.chunk.css"
+    "url": "/project-artifact/static/css/main.33744029.chunk.css"
   },
   {
     "revision": "b3064c46319bb437bb79",
-    "url": "/static/js/1.b3064c46.chunk.js"
+    "url": "/project-artifact/static/js/1.b3064c46.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "/project-artifact/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "4358784e1b58ff53d54dae62a314362e",
-    "url": "/static/media/consumable.4358784e.svg"
+    "url": "/project-artifact/static/media/consumable.4358784e.svg"
   },
   {
     "revision": "b6463ab910f8394b7a7d69e6b25d1ad1",
-    "url": "/static/media/armor.b6463ab9.svg"
+    "url": "/project-artifact/static/media/armor.b6463ab9.svg"
   },
   {
     "revision": "c9cc7951dfd0e0d33e63a939e71f34c5",
-    "url": "/static/media/weapon.c9cc7951.svg"
+    "url": "/project-artifact/static/media/weapon.c9cc7951.svg"
   },
   {
     "revision": "9dd20e9e938ea9dc67b08a8b9cf6f7a1",
-    "url": "/static/media/icon-artifact.9dd20e9e.svg"
+    "url": "/project-artifact/static/media/icon-artifact.9dd20e9e.svg"
   },
   {
     "revision": "5e42fc3efdcc79a7cb1d",
-    "url": "/static/js/main.5e42fc3e.chunk.js"
+    "url": "/project-artifact/static/js/main.5e42fc3e.chunk.js"
   },
   {
     "revision": "fe91e75a434f347e0ea95be40f022ed5",
-    "url": "/static/media/spell.fe91e75a.svg"
+    "url": "/project-artifact/static/media/spell.fe91e75a.svg"
   },
   {
     "revision": "68b5fd4f605775f86f2d143316090684",
-    "url": "/static/media/hero.68b5fd4f.svg"
+    "url": "/project-artifact/static/media/hero.68b5fd4f.svg"
   },
   {
     "revision": "c6cb401499e45b5f6206c65e4f33b747",
-    "url": "/static/media/improvement.c6cb4014.svg"
+    "url": "/project-artifact/static/media/improvement.c6cb4014.svg"
   },
   {
     "revision": "f7a6d44b61151ed037e9029b66f4df3e",
-    "url": "/static/media/health.f7a6d44b.svg"
+    "url": "/project-artifact/static/media/health.f7a6d44b.svg"
   },
   {
     "revision": "0e16ecc68b3cacbbf86a4e8838e71e2a",
-    "url": "/static/media/warning-icon.0e16ecc6.svg"
+    "url": "/project-artifact/static/media/warning-icon.0e16ecc6.svg"
   },
   {
     "revision": "34de4ebfa73d2cd14a123c7533730b18",
-    "url": "/static/media/check-icon.34de4ebf.svg"
+    "url": "/project-artifact/static/media/check-icon.34de4ebf.svg"
   },
   {
     "revision": "f7a6d44b61151ed037e9029b66f4df3e",
-    "url": "/static/media/item.f7a6d44b.svg"
+    "url": "/project-artifact/static/media/item.f7a6d44b.svg"
   },
   {
     "revision": "cb404720772e5cd7bd207854accdab20",
-    "url": "/index.html"
+    "url": "/project-artifact/index.html"
   }
 ];
