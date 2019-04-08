92e2f57aedb470de879b8726c0d050a421f114da
Aleksey Vavilov <aleksey_vavilov@mail.ru>
2019-04-08 08:38:53 +0400

    add test-project

---
+++ test-project/precache-manifest.1ce132ce185dbbb259b1c45464af6b83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9aafdf2e32831771d7e57dcbc81e15d3",
+    "url": "/static/media/icon-up.9aafdf2e.svg"
+  },
+  {
+    "revision": "07f27d1d8fdd8acb6e364f17ebc73ef2",
+    "url": "/static/media/icon-down.07f27d1d.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f5b0b9bbf67258ed98a9",
+    "url": "/static/js/main.97925914.chunk.js"
+  },
+  {
+    "revision": "1224851b5731d0338098",
+    "url": "/static/js/2.5e1b54c4.chunk.js"
+  },
+  {
+    "revision": "f5b0b9bbf67258ed98a9",
+    "url": "/static/css/main.442a20ae.chunk.css"
+  },
+  {
+    "revision": "292146dfc8ecf26226c5c9351d67458a",
+    "url": "/index.html"
+  }
+];
